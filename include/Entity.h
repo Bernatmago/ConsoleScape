21a2c057be9df295e7b02919d1e5944eb94d1f96
BernatMG <bernatmago@gmail.com>
2021-06-19 00:44:47 +0200

    Defined Entity

--- include/Entity.h
+++ include/Entity.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <string>
+#include <list>
+
+using namespace std;
 
 enum EntityType
 {
@@ -11,5 +15,29 @@ enum EntityType
 
 class Entity
 {
+public:
+	Entity(string name, string description, EntityType type);
+	virtual ~Entity();
+
+	string GetName() const;
+	string GetDescription() const;
+	EntityType GetType() const;
+
+	Entity* GetParent() const;
+	void SetParent(Entity* parent);
+
+	void AddEntity(Entity* entity);
+	void RemoveEntity(Entity* entity);
+
+	virtual void PrintDescription();
+	virtual void ShowEntities(EntityType type);
+
+
+protected:
+	string name;
+	string description;
+	EntityType type;
+	Entity* parent;
+	list<Entity*> contains;
 };
 
--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,1 +1,15 @@
 #include "../include/Entity.h"
+
+
+Entity::Entity(string name, string description, EntityType type)
+{
+	this->name = name;
+	this->description = description;
+	this->type = type;
+	this->parent = nullptr;
+}
+
+Entity::~Entity()
+{
+}
+		
